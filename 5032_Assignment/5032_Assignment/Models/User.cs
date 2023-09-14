e590cb5259a01f06e1d4b916b8deb9e9e94ff869
HaowenWang-monash <142271290+HaowenWang-monash@users.noreply.github.com>
2023-09-15 04:22:30 +1000

    new
    new

---
+++ 5032_Assignment/5032_Assignment/Models/User.cs
@@ -0,0 +1,23 @@
+namespace _5032_Assignment.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+    }
+}
